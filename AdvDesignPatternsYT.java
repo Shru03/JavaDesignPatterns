f1e2e8d32443e86253aed2d506dccb4b73bdd298
Shruti Sharma <bhardwaj.shruti03@gmail.com>
2022-04-08 01:32:33 +0530

    Advanced Design Patterns

--- AdvDesignPatternsYT.java
+++ AdvDesignPatternsYT.java
@@ -51,23 +51,6 @@ public class AdvDesignPatternsYT {
 		
 	}
 	
-	public static void hollowSolidRhombus() {
-		int n =5;
-		
-		for (int i =1; i <=n;i++) {
-			int spaces =(n-i);
-			for (int j=1; j <=spaces; j++) {
-				System.out.print("  ");
-			}
-			for (int j=1; j <=n; j++) {
-			//	if (i ==1 || j==1|| i ==n) {
-				System.out.print("* "); 
-			}
-			System.out.println();
-		}
-	}
-	
-	
 	public static void numPyramid() {
 		int n =5;
 		for (int i =1; i <=n;i++) {
